1b55b78aa67ad77745279b59f3252452518d05e9
srabon0 <srabonemam4@gmail.com>
2022-05-22 01:06:00 +0600

    route defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./components/pages/Blogs/Blogs";
+import Home from "./components/pages/Home/Home/Home";
+import Navbar from "./components/Shared/Navbar";
 
 function App() {
   return (
-    <div>
-   
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </>
   );
 }
 
