8be17e67c5c2e1951fa5751b0e140c66990c2ba5
srabon0 <srabonemam4@gmail.com>
2022-05-22 23:07:59 +0600

    business summary and buy us section add

---
+++ src/components/pages/Home/BusinessSummary/BusinessSummary.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-secondary">
+         EntropyLab Business Summary This year
+          </h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            haven't heard of them man bun deep jianbing selfies heirloom prism
+            food truck ugh squid celiac humblebrag.
+          </p>
+        </div>
+        <div class=" flex flex-wrap md:flex md:flex-nowrap  -m-4 text-center stats shadow">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </div>
+            <div class="stat-title">Downloads</div>
+            <div class="stat-value">31K</div>
+            <div class="stat-desc">Jan 1st - Feb 1st</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                ></path>
+              </svg>
+            </div>
+            <div class="stat-title">New Users</div>
+            <div class="stat-value">4,200</div>
+            <div class="stat-desc">↗︎ 400 (22%)</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+                ></path>
+              </svg>
+            </div>
+            <div class="stat-title">New Registers</div>
+            <div class="stat-value">1,200</div>
+            <div class="stat-desc">↘︎ 90 (14%)</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <div class="avatar online">
+                <div class="w-16 rounded-full">
+                  <img src="https://api.lorem.space/image/face?w=128&h=128" />
+                </div>
+              </div>
+            </div>
+            <div class="stat-value">86%</div>
+            <div class="stat-title">Tasks done</div>
+            <div class="stat-desc text-secondary">31 tasks remaining</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BusinessSummary;
--- src/components/pages/Home/Home/Home.js
+++ src/components/pages/Home/Home/Home.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PageTitle from '../../../Shared/PageTitle';
 import Banner from '../Banner/Banner'
 import WhyUs from '../WhyUs/WhyUs';
+import Buy from '../Buy/Buy';
+import BusinessSummary from '../BusinessSummary/BusinessSummary';
 const Home = () => {
     return (
         <>
          <PageTitle title="Home"></PageTitle>
           <Banner></Banner>
+          <Buy></Buy>
           <WhyUs></WhyUs>
+          <BusinessSummary></BusinessSummary>
+          
         </>
     );
 };
