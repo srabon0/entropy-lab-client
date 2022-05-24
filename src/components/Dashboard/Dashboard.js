3be71c9fface09a6c774d7a648d6ba746007c7e0
srabon0 <srabonemam4@gmail.com>
2022-05-24 10:56:14 +0600

    cancelled order

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -20,29 +20,52 @@ const Dashboard = () => {
     <div class="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
+        <label
+          for="my-drawer-2"
+          class=" mx-auto btn w-52 btn-xs btn-outline btn-accent text-white drawer-button lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Click to Expand
+        </label>
         <h1 className="text-blue-500 text-4xl font-medium">
           Welcome to dashboard
         </h1>
         {/* <!-- Page content here --> */}
+
         <Outlet />
       </div>
       <div class="drawer-side">
         <label htmlFor="my-drawer-2" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-          {
-            user && <>
-            <li>
-              <Link to="/dashboard">My Profile</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/myorders">My orders</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/addreview">Add Review</Link>
-            </li>
-
+          {user && (
+            <>
+              <li>
+                <Link to="/dashboard">My Profile</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/myorders">My orders</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/addreview">Add Review</Link>
+              </li>
             </>
-          }
+          )}
 
           {/* <!-- Sidebar content here --> */}
 
--- src/components/Dashboard/Myorders.js
+++ src/components/Dashboard/Myorders.js
@@ -1,42 +1,117 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
+import { Confirm } from "react-st-modal";
+import { toast } from "react-toastify";
+import { useQuery } from "react-query";
 const Myorders = () => {
-    const [user, loading, error] = useAuthState(auth);
-  
-    const [orders,setOrders] = useState([]);
-    
-    useEffect(()=>{
-        const getMyOrders = async()=>{
-            const url =` http://localhost:5000/order/${user.email}`
-            const {data} = await axios.get(url)
-            setOrders(data)
-        }
-        if(user){
-            getMyOrders()
-        }
-
-
-    },[])
-
-    if (loading) {
-        return <Loading></Loading>;
-      }
-      if (error) {
-        return (
-          <div>
-            <p>Error: {error}</p>
-          </div>
-        );
-      }
+  const [user, loading, error] = useAuthState(auth);
+  const url = ` http://localhost:5000/order/${user.email}`;
+  const { isLoading, error2, data:orders,refetch } = useQuery('orders', () =>
+     fetch(url).then(res =>
+       res.json()
+     )
+   )
+ 
+  if (loading || isLoading) {
+    return <Loading></Loading>;
+  }
+  if (error || error2) {
     return (
-        <div>
-           {orders.length}
-        </div>
+      <div>
+        <p>Error: {error || error2}</p>
+      </div>
     );
+  }
+  return (
+    <section class="container mx-auto p-6">
+      <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+        <div class="w-full overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr class="text-md tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                <th class="px-3 py-2">#SL</th>
+                <th class="px-3 py-2">Product Name</th>
+                <th class="px-3 py-2">Order Quantity</th>
+                <th class="px-3 py-2">Price Per Unit</th>
+                <th class="px-3 py-2">Pay Total</th>
+                <th class="px-3 py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody class="bg-white">
+              {orders.map((order, index) => (
+                <OrderRow
+                refetch={refetch}
+                
+                  index={index}
+                  key={order._id}
+                  order={order}
+                ></OrderRow>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Myorders;
+
+
+const OrderRow = ({refetch, order, index }) => {
+  const { _id, productName, orderQ, pricePerUnit } = order;
+
+  const cancelOrder = async(id) =>{
+    const url = `http://localhost:5000/item/${id}`
+    const headers = {
+        "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+    }
+    const result = await Confirm(
+        "Are you Sure You want to cancel the order?",
+        "Warning"
+      );
+
+      if (result) {
+
+        const {data} = axios.delete(url,{headers:headers})
+            toast.success("Order cancelled")
+            refetch();
+           
+            
+        
+
+        
+      } else {
+        // Сonfirmation not confirmed
+    
+      }
+
+  } 
+  return (
+    <>
+      <tr class="text-gray-700">
+        <td class="px-3 py-2 border">{index + 1}</td>
+        <td class="px-3 py-2 text-ms  border">{productName}</td>
+        <td class="px-3 py-2 text-xs border">
+          <span class="px-3 py-1 leading-tight text-green-700 bg-green-100 rounded-sm">
+            {orderQ}
+          </span>
+        </td>
+        <td class="px-3 py-2 text-sm border">{pricePerUnit}</td>
+        <td class="px-3 py-2 text-sm border">{orderQ * pricePerUnit}</td>
+        <td class="px-3 py-2 text-sm border">
+          <button className="btn btn-sm btn-success mx-1">Pay</button>
+          <button
+            onClick={ () => cancelOrder(_id) }
+            className="btn btn-sm btn-error mx-1"
+          >
+            Cancel Order
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
